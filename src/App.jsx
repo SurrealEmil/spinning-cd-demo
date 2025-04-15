5bf731291f2fa5730b927c09ab8e51332934e03d
SurrealEmil <emil.ejderklev1@hotmail.com>
2025-04-15 12:28:30 +0200

    Add zoom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Canvas } from '@react-three/fiber';
 import { useGLTF } from '@react-three/drei';
+import { OrbitControls } from '@react-three/drei';
 import { useRef, useState } from 'react';
 
 function CDModel({ rotationX, rotationY }) {
@@ -71,11 +72,17 @@ function App() {
       onTouchEnd={handleTouchEnd}
       style={{ width: '100vw', height: '100vh', cursor: 'grab', touchAction: 'none' }}
     >
-      <Canvas camera={{ position: [0, 0, 2] }}>
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[0, 5, 5]} />
-        <CDModel rotationX={rotationX} rotationY={rotationY} />
-      </Canvas>
+    <Canvas camera={{ position: [0, 0, 2], fov: 50 }}>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[0, 5, 5]} />
+      <CDModel rotationX={rotationX} rotationY={rotationY} />
+      <OrbitControls
+        enableRotate={false} // disable built-in rotation
+        enableZoom={true}    // enable zoom with scroll or pinch
+        minDistance={1}
+        maxDistance={5}
+      />
+    </Canvas>
     </div>
   );
 }
